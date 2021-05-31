20b259bd60f351b97834dc22222028d0901f996b
ranjitgouda <ranjitgouda@gmail.com>
2021-05-31 17:52:25 +1000

    Add files via upload

--- run.sh
+++ run.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
-no_of_lines=$1
-echo "Building docker image"
+if [ $# -eq 0 ];then
+  echo "File line count not provided, adding default value of 1000 lines"
+  no_of_lines=1000
+else
+  no_of_lines=$1
+fi
+
+echo "Building docker image $no_of_lines}"
 docker build -t json_parser .
 
 echo "Running docker container"
